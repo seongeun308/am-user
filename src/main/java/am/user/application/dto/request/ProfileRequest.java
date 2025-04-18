1fc16b21cfc7e2761301fc08321c6e43a52871d3
seongeun308 <kse011010@gmail.com>
2025-04-19 00:21:06 +0900

    AM-15 (feat): 회원가입 정보 유효성 검사
    - 요청 데이터 유효성 검사
    - validation 의존성 추가

--- src/main/java/am/user/application/dto/request/ProfileRequest.java
+++ src/main/java/am/user/application/dto/request/ProfileRequest.java
@@ -1,10 +1,13 @@
 package am.user.application.dto.request;
 
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class ProfileRequest {
+    @NotBlank(message = "닉네임은 필수 입력값입니다.")
+    @Pattern(regexp = "^[a-z0-9_]{1,30}$", message = "닉네임은 영문 대소문자, 숫자, 특수문자만 가능하며 최대 30자 입니다")
     private String nickname;
     private String bio;
 }
