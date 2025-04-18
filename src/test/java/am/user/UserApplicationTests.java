abe092a9f167012834627d66340515f5dd55d20f
seongeun308 <kse011010@gmail.com>
2025-04-18 19:51:14 +0900

    initialize spring boot project

---
+++ src/test/java/am/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package am.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
